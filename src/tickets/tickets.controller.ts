f8ac1b9d9a81dc154f0c935a645f27e4cc7eae5a
TalhaShahidKhan <talhashahidkhan49@gmail.com>
2026-09-19 23:54:09 +0600

    Get all tickets done

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { TicketsService } from './tickets.service.js';
 
 @Controller('tickets')
 export class TicketsController {
-    @Get()
-      findAll() {
-        return [];
-      }
+  constructor(private readonly ticketService:TicketsService){}
+  @Get()
+  findAll() {
+    return this.ticketService.findAll()
+  }
 }

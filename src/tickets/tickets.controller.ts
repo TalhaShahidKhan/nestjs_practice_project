ea69fa0370dfbd5a1cc218fd77d8f4dd656f81a8
TalhaShahidKhan <talhashahidkhan49@gmail.com>
2026-09-21 00:12:44 +0600

    Guard and update ticket

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { TicketsService } from './tickets.service.js';
 import { CreateTicketDto } from './dto/create-ticket.dto.js';
 import { FilterTicketQueryDto } from './dto/filter-ticket-query.dto.js';
+import { UpdateTicketDto } from './dto/update-ticket.dto.js';
+import { StaffGuard } from './guards/staff.guard.js';
 
 @Controller('tickets')
 export class TicketsController {
@@ -17,5 +19,14 @@ export class TicketsController {
   @Post()
   create(@Body() createTicketDto:CreateTicketDto){
     return this.ticketService.create(createTicketDto)
+  }
+  @Patch(':id')
+  update(@Param('id',ParseIntPipe) id:number,@Body() updateTicketDto:UpdateTicketDto){
+    return this.ticketService.update(id,updateTicketDto)
+  }
+  @UseGuards(StaffGuard)
+  @Patch(':id/close')
+  closeTicket(@Param('id',ParseIntPipe) id:number){
+    return this.ticketService.closeTicket(id)
   }
 }
--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,9 +1,14 @@
-import { Module,Get } from '@nestjs/common';
+import { Module,Get, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { TicketsController } from './tickets.controller.js';
 import { TicketsService } from './tickets.service.js';
+import { RequestLoggerMiddleware } from '../common/request-logger.middleware.js';
 
 @Module({
   controllers: [TicketsController],
   providers: [TicketsService]
 })
-export class TicketsModule {}
+export class TicketsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggerMiddleware).forRoutes(TicketsController)
+  }
+}
